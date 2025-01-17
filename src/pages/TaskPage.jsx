ed28e1581aced4e0311d2933e62c9333d34691cd
Matheus Bezerra <matheusbzrr3@gmail.com>
2025-01-17 17:23:24 -0300

    ok

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -8,7 +8,7 @@ function TaskPage() {
   const title = searchParams.get("title");
   const descricao = searchParams.get("descricao");
   return (
-    <div className="h-screen w-screen bg-slate-500 p-6">
+    <div className="h-screen w-screen bg-slate-500 p-6 flex justify-center">
       <div className="w-[500px] space-y-4">
         <div className="flex justify-center relative mb-6">
           <button
